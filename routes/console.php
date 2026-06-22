5d41bcb7eb376e9de31bc3ba9b5a4915f039e2f2
Administrador Fuego Vivo <admin@fuegovivo.com.co>
2026-06-22 11:38:07 -0500

    chore: agrega output de logs para los comandos programados

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,10 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reports:send-reminders')->hourly();
-Schedule::command('metrics:recalculate-all')->dailyAt('00:05');
+Schedule::command('reports:send-reminders')
+    ->hourly()
+    ->appendOutputTo(storage_path('logs/cron.log'));
+
+Schedule::command('metrics:recalculate-all')
+    ->dailyAt('00:05')
+    ->appendOutputTo(storage_path('logs/cron.log'));
